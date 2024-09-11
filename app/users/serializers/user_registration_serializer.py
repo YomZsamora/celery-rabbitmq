1061bf337f49ef6da06d700aff056cf51813e01e
yomZsamora <samaurah@gmail.com>
2024-09-11 16:54:58 +0300

    Incorporated last_login read only field to UserRegistrationSerializer.

--- app/users/serializers/user_registration_serializer.py
+++ app/users/serializers/user_registration_serializer.py
@@ -6,6 +6,10 @@ from notifications.services import RegistrationEmailNotification
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     
+    """
+    Serializer for handling user registration.
+    """
+    
     last_name = serializers.CharField()
     first_name = serializers.CharField()
     email = serializers.EmailField(max_length=100, validators=[email_validator])
@@ -18,6 +22,12 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         
+        """
+        Create a new user with the provided validated data.
+        Args: validated_data (dict): Validated data for user creation.
+        Returns: User: Newly created user instance.
+        """
+        
         with transaction.atomic():
             user = User(email=validated_data['email'], first_name=validated_data['first_name'], last_name=validated_data['last_name'])
             user.set_password(validated_data['password'])
@@ -27,6 +37,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         return user
 
     def send_notification(self, user: User) -> None:
+        
+        """
+        Send registration notification email to the user.
+        Args: user (User): The user object to notify.
+        """
         
         email_service = RegistrationEmailNotification(user)
         email_service.send_registration_notification()
