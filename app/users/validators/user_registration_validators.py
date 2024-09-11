48174fa693d81eb9ffb084a7147bc7a82f26a8ac
yomZsamora <samaurah@gmail.com>
2024-09-11 17:29:15 +0300

    Updated password validator check.

--- app/users/validators/user_registration_validators.py
+++ app/users/validators/user_registration_validators.py
@@ -2,17 +2,11 @@ from users.models import User
 from utils.exceptions.custom_exceptions import SerializerValidationsError
 
 def email_validator(email: str):
-    exists = User.objects.filter(email=email).exists()
-    if exists:
-        raise SerializerValidationsError(f"{email} already has an account registered.")
-    return email    
+    if User.objects.filter(email=email).exists():
+        raise SerializerValidationsError(f"{email} already has an account.")
+    return email
 
 def password_validator(password: str):
-    requirements = {
-        'digit': any(char.isdigit() for char in password),
-        'lower_case': any(char.islower() for char in password),
-        'upper_case': any(char.isupper() for char in password)
-    }
-    if not all(requirements.values()):
-        raise SerializerValidationsError("Kindly provide a strong password.")
+    if not (any(c.isdigit() for c in password) and any(c.islower() for c in password) and any(c.isupper() for c in password)):
+        raise SerializerValidationsError("Password must contain at least one uppercase, lowercase, and digit.")
     return password
