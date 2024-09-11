69edcbe01399d159bc8094c340e70a68600cee84
yomZsamora <samaurah@gmail.com>
2024-09-11 16:53:26 +0300

    Updated used DEFAULT_FROM_EMAIL.

--- app/notifications/services/email_service.py
+++ app/notifications/services/email_service.py
@@ -5,6 +5,15 @@ from django.core.mail import EmailMessage
 @shared_task
 def send_email(mail_data: dict[str, any]) -> None:
     
+    """
+    Asynchronous task that sends an email using Django's EmailMessage class.
+    Args: mail_data (dict): A dictionary containing email data.
+    - 'subject': Subject of the email (str)
+    - 'message': Body of the email (str)
+    - 'recipients': List of recipient email addresses (list)
+    Returns: None
+    """
+    
     email = EmailMessage(
         mail_data['subject'], 
         mail_data['message'], 
