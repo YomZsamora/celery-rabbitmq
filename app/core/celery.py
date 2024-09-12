38a62d73559c147175f096c9b111891c38701276
yomZsamora <samaurah@gmail.com>
2024-09-12 12:12:35 +0300

    Updated celery to use beat scheduler.

--- app/core/celery.py
+++ app/core/celery.py
@@ -6,3 +6,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 celery_app = Celery("core")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
+celery_app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
