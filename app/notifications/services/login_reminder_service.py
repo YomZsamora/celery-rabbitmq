b4b068482eea41ec147af14dbfeed8ec6b63564b
yomZsamora <samaurah@gmail.com>
2024-09-13 08:24:35 +0300

    Refactored code for fetching users without login details.

--- app/notifications/services/login_reminder_service.py
+++ app/notifications/services/login_reminder_service.py
@@ -5,11 +5,16 @@ from notifications.services import send_email
 @shared_task
 def check_last_login() -> None:
     
+    """
+    Celery task to check for inactive users with no last login and send them a reminder email.
+    Returns: None
+    """
+    
     inactive_users = User.objects.filter(last_login__isnull=True)
     inactive_users_emails = [user.email for user in inactive_users]
     mail_data = {
         "subject": "Pending Account Confirmation",
-        "message": "Reminder to login tp your account and finalize registration",
+        "message": "Please ensure you log in to your account and complete the registration process.",
         "recipients": inactive_users_emails
     }
     send_email.delay(mail_data)
